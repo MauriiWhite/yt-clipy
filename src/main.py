db7a6b27c8ee0613f71dbed19e1453690abf1e73
MauriiWhite <mauriblanco.s@gmail.com>
2025-01-02 02:50:11 -0300

    feat: creation of the base for the main screen

--- src/main.py
+++ src/main.py
@@ -1,11 +1,31 @@
-from search import VideoSearcher
+# from search import VideoSearcher
+
+# if __name__ == "__main__":
+#     searcher = VideoSearcher(limit=5)
+#     query = input("Enter a search query: ").strip()
+#     videos = searcher.search(query)
+
+#     if videos:
+#         print("Videos found:")
+#         for idx, video in enumerate(videos, start=1):
+#             print(f"{idx}. {video.title} - {video.url}")
+
+from textual.app import App, ComposeResult
+from textual.screen import Screen
+from textual.widgets import Header, Input
+
+
+class SearchScreen(Screen):
+    def compose(self) -> ComposeResult:
+        yield Header(name="YTClipy", show_clock=True, time_format="%H:%M")
+        yield Input(placeholder="Search for YouTube videos")
+
+
+class YTClipy(App):
+    def on_ready(self) -> None:
+        self.push_screen(SearchScreen())
+
 
 if __name__ == "__main__":
-    searcher = VideoSearcher(limit=5)
-    query = input("Enter a search query: ").strip()
-    videos = searcher.search(query)
-
-    if videos:
-        print("Videos found:")
-        for idx, video in enumerate(videos, start=1):
-            print(f"{idx}. {video.title} - {video.url}")
+    app = YTClipy()
+    app.run()
